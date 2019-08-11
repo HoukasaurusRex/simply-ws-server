cea5f0eadb2632db2f2b7f94d7aeb1c7af4205f6
Pterobyte <jt1992@gmail.com>
2019-08-11 22:54:02 +0800

    fixed precommit

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,4 +1,3 @@
 module.exports = {
-  "*.{js,vue}": "eslint --fix",
-  // "*.scss": "stylelint --syntax=scss" // Dear Jonas, when you feel fit of health and strong of heart, uncomment this line
+  "*.js": "eslint --fix"
 }
